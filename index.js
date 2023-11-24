a9acd9d9fa11ec9bdaaa697100411c1a3331f286
torreymr <torreymramirez@gmail.com>
2023-11-24 10:19:30 -0700

    Added functionallity to title of notepad

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const toggleButton = function() {
     const button = document.getElementById("toggleButton");
     button.classList.toggle("pad__checked");
 }
+
+const enterPressed = function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("pad__title").classList.add("enter-pressed");
+        document.getElementById("pad__title").blur();
+    }
+}
+
+const editTitle = function() {
+    document.getElementById("pad__title").classList.remove("enter-pressed");
+}
+
