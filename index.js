c01755b90cfae37b3430dae39be438f1e377bc46
torreymr <torreymramirez@gmail.com>
2023-11-24 10:46:17 -0700

    Added functionallity to input tag of task name

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ const editTitle = function() {
     document.getElementById("pad__title").classList.remove("enter-pressed");
 }
 
+const taskCreated = function(event){
+if (event.key === "Enter") {
+    document.getElementById("task__name").classList.add("enter-pressed");
+    document.getElementById("task__name").blur();
+    document.getElementById("deleteButton").classList.add("delete__button");
+}}
+
+const editTask = function (){
+    document.getElementById("task__name").classList.remove("enter-pressed");
+}
