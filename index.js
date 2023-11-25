37941c0789c2f4236521c3dcd8819df3bd21e8bf
torreymr <torreymramirez@gmail.com>
2023-11-24 17:18:15 -0700

    Added transition effects on input tag

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "use strict"
+
+
+const enterTitle = function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("listTitle").classList.add("entered__title");
+        document.getElementById("listTitle").blur();
+    }
+}
+
+const editTitle = function() {
+    document.getElementById("listTitle").classList.remove("entered__title");
+}
