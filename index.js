bcced5bff7f454d0e552587e34d7a8d2752ce112
torreymr <torreymramirez@gmail.com>
2023-11-24 18:13:12 -0700

    Added transition effect when input tag is closed

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 "use strict"
-
+const listTitle = document.getElementById("listTitle");
 
 const enterTitle = function(event) {
     if (event.key === "Enter") {
-        document.getElementById("listTitle").classList.add("entered__title");
-        document.getElementById("listTitle").blur();
+        listTitle.classList.add("entered__title");
+        listTitle.blur();
     }
-}
+};
+
 
 const editTitle = function() {
-    document.getElementById("listTitle").classList.remove("entered__title");
-}
+    listTitle.classList.remove("entered__title");
+};
+
+listTitle.addEventListener("blur", function(){
+    listTitle.classList.add("entered__title");
+});
