771e1312694dcb141de917d341761a92cf7e6dd6
torreymr <torreymramirez@gmail.com>
2023-11-30 11:05:39 -0700

    Made new task button work (kind of)

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 "use strict";
 
 const listTitleInput = document.getElementById("listTitleInput");
+const addTaskButton = document.getElementById("addTask");
+const listTasks = document.getElementById("listTasks");
 
 listTitleInput.addEventListener("keydown", function(event){
     if(event.key === "Enter"){
         listTitleInput.blur();
     }
-})
+});
+
+
+const addTask = function(){
+    console.log("Button Clicked!")
+    const task = document.createElement("div");
+    const checkBox = document.createElement("button");
+    const taskName = document.createElement("input");
+    const deleteButton = document.createElement("button");
+
+    task.appendChild(checkBox);
+    task.appendChild(taskName);
+    task.appendChild(deleteButton);
+
+    listTasks.appendChild(task);
+};
+
