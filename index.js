3df0abe2d13de0af9d08e8a857f50307778fb530
torreymr <torreymramirez@gmail.com>
2023-12-01 15:47:22 -0700

    Icon added on deleteTaskButton through JS

--- index.js
+++ index.js
@@ -54,11 +54,15 @@ document.getElementById("newTask").addEventListener("click",function(){
     const deleteTaskButton = document.createElement("button");
     deleteTaskButton.setAttribute("id", "deleteTask");
 
+    const deleteTaskIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+  </svg>`
+
     task.appendChild(checkTab);
     task.appendChild(taskNameContainer);
     taskNameContainer.appendChild(taskNameInput);
     taskNameContainer.appendChild(deleteTaskButton);
-
+    deleteTaskButton.innerHTML = deleteTaskIcon;
     document.getElementById("listTasksContainer").appendChild(task);
 });
 
