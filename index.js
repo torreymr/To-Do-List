2f7424e84c714a9d35382b086153f266dd77df93
torreymr <torreymramirez@gmail.com>
2023-12-01 15:17:42 -0700

    Actually working now

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 "use strict";
-const listTitleInput = document.getElementById("listTitle");
-const charCount = document.getElementById("charCount");
-const currentLength = listTitleInput.value.length;
-document.addEventListener("DOMContentLoaded", function(){
 
+
+document.addEventListener("DOMContentLoaded", function(){
+    const listTitleInput = document.getElementById("listTitle");
+    const charCount = document.getElementById("charCount");
 
     const maxCharacters = 15;
 
     listTitleInput.addEventListener("input", function(){
-
+        const currentLength = listTitleInput.value.length;
         if (currentLength > 0){
             charCount.classList.remove("hidden");
             charCount.textContent = currentLength + "/" + maxCharacters;
         }
 
-    })
-});
-
-listTitleInput.addEventListener("keydown", function(event){
-    if(event.key === "Enter"){
-        listTitleInput.blur();
-        listTitleInput.classList.add("entered")
+    });
+    listTitleInput.addEventListener("keydown", function(event){
+        if(event.key === "Enter"){
+            listTitleInput.blur();
+            listTitleInput.classList.add("entered")
+            charCount.classList.add("hidden");
+        }
+    });
+    
+    listTitleInput.addEventListener("click", function(){
+        const currentLength = listTitleInput.value.length;
+        listTitleInput.classList.remove("entered");
+        charCount.classList.remove("hidden");
+        charCount.textContent = currentLength + "/" + maxCharacters;
+    });
+    
+    listTitleInput.addEventListener("blur", function(){
+        if (listTitleInput.value.length > 0){
+            listTitleInput.classList.add("entered")
+        }
         charCount.classList.add("hidden");
-    }
+    });
 });
 
-listTitleInput.addEventListener("click", function(){
-    listTitleInput.classList.remove("entered");
-    charCount.classList.remove("hidden");
-    charCount.textContent = currentLength + "/" + maxCharacters;
-});
 
-listTitleInput.addEventListener("blur", function(){
-    if (listTitleInput.value.length > 0){
-        listTitleInput.classList.add("entered")
-    }
-    charCount.classList.add("hidden");
-})
 
