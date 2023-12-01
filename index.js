9a7eaf2d61db50cee89257f57c6cdf1b5b4718ec
torreymr <torreymramirez@gmail.com>
2023-12-01 13:30:41 -0700

    Added functionallity to charCount

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", function(){
+    const listTitleInput = document.getElementById("listTitle");
+    const charCount = document.getElementById("charCount");
+    const maxCharacters = 15;
 
+    listTitleInput.addEventListener("input", function(){
+        const currentLength = listTitleInput.value.length;
+        if (currentLength > 0){
+            charCount.classList.remove("hidden");
+            charCount.textContent = currentLength + "/" + maxCharacters;
+        } else if (currentLength == 0){
+            charCount.classList.add("hidden");
+        }
+
+    })
+})
