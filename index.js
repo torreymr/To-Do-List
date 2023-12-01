7c74446bee50243c1fb414de05b587e8d9e33cb3
torreymr <torreymramirez@gmail.com>
2023-12-01 15:15:33 -0700

    Fixed charCount again

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict";
 const listTitleInput = document.getElementById("listTitle");
 const charCount = document.getElementById("charCount");
-
+const currentLength = listTitleInput.value.length;
 document.addEventListener("DOMContentLoaded", function(){
 
 
     const maxCharacters = 15;
 
     listTitleInput.addEventListener("input", function(){
-        const currentLength = listTitleInput.value.length;
+
         if (currentLength > 0){
             charCount.classList.remove("hidden");
             charCount.textContent = currentLength + "/" + maxCharacters;
