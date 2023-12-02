b85f27fcd9f136e03b2bf8bc63e8c76ab2d1dbca
torreymr <torreymramirez@gmail.com>
2023-12-02 11:38:07 -0700

    Added functionallity to input

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 "use strict";
 
+const activeInput = function(inputElement){
+    const defaultInputBG = "rgba(120, 120, 120, 0.3)";
+    const activeInputBG = "rgba(120, 120, 120, 0.1)";
+
+    const defaultInputC = "rgba(191, 191, 191, .1)";
+    const enteredInputC = "rgba(191, 191, 191, 1)";
+
+    inputElement.style.color = defaultInputC;
+    inputElement.style.backgroundColor = activeInputBG;
+
+    inputElement.addEventListener("blur", function(){
+        const inputElementLength = inputElement.value.length; 
+        if (inputElementLength === 0){
+            inputElement.style.backgroundColor = defaultInputBG;
+        } else {
+            inputElement.style.color = enteredInputC;
+        }
+    });
+
+
+    inputElement.addEventListener("keydown", function(event){
+
+        const inputElementLength = inputElement.value.length; 
+
+        if (event.key  === "Enter") {
+            if (inputElementLength > 0) {
+                inputElement.style.color = enteredInputC;
+            } else {
+                inputElement.style.color = defaultInputC;
+            }
+        }
+    })
+
+
+};
+
 
 
