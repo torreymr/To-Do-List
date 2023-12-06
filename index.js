28da80b2a264609d748f660b4f06fcd9f0643c0d
torreymr <torreymramirez@gmail.com>
2023-12-05 19:59:46 -0700

    Retrieved name of the user and stored it in a variable

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("greetingsPage.ejs");
 });
 
 app.post("/home", (req, res) => {
-  res.send("<h1>Home Page</h1>");
+  const nameOfUser = req.body.username;
+  res.send(`<h1>Your Name is ${nameOfUser}!</h1>`);
 });
 
 app.listen(port, () => {
