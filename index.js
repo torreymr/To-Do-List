8a446ff4bee219b78cb5adc84e436d9a7d37c04b
torreymr <torreymramirez@gmail.com>
2023-12-05 19:22:00 -0700

    Added functionallity to greetingsPage button

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import bodyParser from "body-parser";
 import express from "express";
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/home", (req, res) => {
+  res.send("<h1>Home Page</h1>");
 });
 
 app.listen(port, () => {
